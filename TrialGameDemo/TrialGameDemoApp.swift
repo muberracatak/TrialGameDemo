c670ed460d7dc50d0ddd8252fa4508536be54c6a
muberracatak <55498680+muberracatak@users.noreply.github.com>
2022-06-26 15:22:32 +0300

    Initial Commit

---
+++ TrialGameDemo/TrialGameDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrialGameDemoApp.swift
+//  TrialGameDemo
+//
+//  Created by Müberra Çatak on 26.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrialGameDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
